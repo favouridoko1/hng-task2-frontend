106f2dee521313d25fb49c9481c2596d5cad05a7
Favour Idoko <ojodomofavour2004@gmail.com>
2024-07-09 15:46:52 +0100

    Commiting changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Cart from "./Components/CartPage/Cart";
 import Product from "./Components/Product/Product";
 const App = () => {
   return (
     <div className="app">
+      <button onClick={Navigate('/products')}>To Product</button>
       <Routes>
         <Route path="/products" element={ <Product />} />
         <Route path="/cart" element={<Cart />} />
